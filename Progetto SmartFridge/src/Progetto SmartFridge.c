1c68e48dbd33c77e3437a55cc7fca2b50833e1c0
VitandreaSorino <vitandrea98@live.it>
2018-05-16 12:07:34 +0200

    Aggiunta del file Tipi_Dato.h con tutte le strutture principali

--- Progetto SmartFridge/src/Progetto SmartFridge.c
+++ Progetto SmartFridge/src/Progetto SmartFridge.c
@@ -4,7 +4,7 @@
  Author      : Vitandrea Sorino
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Hel1lo World in C, Ansi-style
  ============================================================================
  */
 
